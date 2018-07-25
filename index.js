07222f963417f29890a614e8fbf7e7febc4ab334
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 19:31:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@
 function driversWithRevenueOver(arr, revenue){
     return arr.filter(function(driver){ return driver.revenue > revenue})
 }
+
+
+function driverNamesWithRevenueOver(arr, revenue){
+    newArr = arr.filter(function(driver){ return driver.revenue > revenue})
+    return newArr.map(function(driver){return driver.name})
+}
