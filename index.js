3145f251846efed3a4d446a26c6dd13cc77fcf3f
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 19:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution here:
 
 function driversWithRevenueOver(arr, revenue){
-  arr.filter(function(driver)driver.revenue > revenue)
+    return arr.filter(function(driver)driver.revenue > revenue)
 }
