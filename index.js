b82238324ee5dc5b0a36049417cc91cf5a1d9621
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 19:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,11 @@ function driverNamesWithRevenueOver(arr, revenue){
     newArr = arr.filter(function(driver){ return driver.revenue > revenue})
     return newArr.map(function(driver){return driver.name})
 }
+
+function exactMatch(arr, object){
+  return arr.filter(function(driver){
+      for(const key in object){
+        driver[key] == object[key]
+}
+   })
+}
